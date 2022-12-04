87c9fa0333069b90693fa15b0e25db62b729ff6a
Maroš Hanzel <maros.hanzel@accenture.com>
2022-12-04 19:37:44 +0100

    day 2 refactoring

--- day-2/src/main/java/sk/mha/aoc/day2/RPSTournament.java
+++ day-2/src/main/java/sk/mha/aoc/day2/RPSTournament.java
@@ -3,6 +3,7 @@ package sk.mha.aoc.day2;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 public class RPSTournament {
@@ -12,18 +13,17 @@ public class RPSTournament {
     }
 
     public static long playWithIncorrectRules(Path inputFilePath) throws IOException {
-        try (Stream<String> lines = Files.lines(inputFilePath)) {
-            return lines.map(line -> line.split(" "))
-                .map(parts -> new RPSMatch(Hand.get(parts[0]), Hand.get(parts[1])))
-                .mapToLong(match -> match.yourHand().getRewardPoints() + match.outcome().getRewardPoints())
-                .sum();
-        }
+        return play(inputFilePath, parts -> new RPSMatch(Hand.get(parts[0]), Hand.get(parts[1])));
     }
 
     public static long playWithCorrectRules(Path inputFilePath) throws IOException {
+        return play(inputFilePath, parts -> RPSMatch.forOutcome(Hand.get(parts[0]), Outcome.get(parts[1])));
+    }
+
+    private static long play(Path inputFilePath, Function<String[], RPSMatch> rpsMatchFunction) throws IOException {
         try (Stream<String> lines = Files.lines(inputFilePath)) {
             return lines.map(line -> line.split(" "))
-                .map(parts -> RPSMatch.forOutcome(Hand.get(parts[0]), Outcome.get(parts[1])))
+                .map(rpsMatchFunction)
                 .mapToLong(match -> match.yourHand().getRewardPoints() + match.outcome().getRewardPoints())
                 .sum();
         }
