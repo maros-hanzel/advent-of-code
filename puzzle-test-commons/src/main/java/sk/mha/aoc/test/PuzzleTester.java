21bb3ec500c95dc83853aa26d76d81c7c642d185
Maroš Hanzel <maros.hanzel@accenture.com>
2022-12-05 17:50:05 +0100

    putting answer assertion into the puzzle tester

--- puzzle-test-commons/src/main/java/sk/mha/aoc/test/PuzzleTester.java
+++ puzzle-test-commons/src/main/java/sk/mha/aoc/test/PuzzleTester.java
@@ -6,19 +6,21 @@ import sk.mha.aoc.api.PuzzleAPI;
 import java.util.List;
 import java.util.function.Supplier;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public final class PuzzleTester {
 
     private PuzzleTester() {
         // private constructor to prevent instantiation
     }
 
-    public static <T> T test(@NonNull Supplier<PuzzleAPI<T>> puzzleSupplier, @NonNull String inputFile) {
+    public static <T> void test(@NonNull Supplier<PuzzleAPI<T>> puzzleSupplier, @NonNull String inputFile, @NonNull T expectedAnswer) {
         PuzzleAPI<T> puzzle = puzzleSupplier.get();
         if (null == puzzle) {
             throw new IllegalArgumentException("PuzzleSupplier must not return null");
         }
         List<String> input = TestInputLoader.loadTestInput(inputFile);
-        return puzzle.solve(input);
+        assertEquals(expectedAnswer, puzzle.solve(input));
     }
 
 }
