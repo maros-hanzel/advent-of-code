7a1214e48a8c670fc3853422f76aa8f6155b5647
Maroš Hanzel <maros.hanzel@accenture.com>
2023-02-08 00:21:34 +0100

    using lombok to define private constructor in PuzzleTester

--- puzzle-test-commons/src/main/java/sk/mha/aoc/test/PuzzleTester.java
+++ puzzle-test-commons/src/main/java/sk/mha/aoc/test/PuzzleTester.java
@@ -1,28 +1,29 @@
 package sk.mha.aoc.test;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import lombok.NonNull;
 import sk.mha.aoc.api.PuzzleAPI;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.function.Supplier;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class PuzzleTester {
-
-    private PuzzleTester() {
-        // private constructor to prevent instantiation
-    }
 
     public static <T> void test(Supplier<PuzzleAPI<T>> puzzleSupplier, T expectedAnswer) {
         test(puzzleSupplier, "test.input", expectedAnswer);
     }
 
-    public static <T> void test(@NonNull Supplier<PuzzleAPI<T>> puzzleSupplier, @NonNull String inputFile, @NonNull T expectedAnswer) {
-        PuzzleAPI<T> puzzle = puzzleSupplier.get();
-        if (null == puzzle) {
-            throw new IllegalArgumentException("PuzzleSupplier must not return null");
-        }
+    public static <T> void test(
+        @NonNull Supplier<PuzzleAPI<T>> puzzleSupplier,
+        @NonNull String inputFile,
+        @NonNull T expectedAnswer
+    ) {
+        PuzzleAPI<T> puzzle = Objects.requireNonNull(puzzleSupplier.get(), "PuzzleSupplier must not return null");
         List<String> input = TestInputLoader.loadTestInput(inputFile);
         assertEquals(expectedAnswer, puzzle.solve(input));
     }
